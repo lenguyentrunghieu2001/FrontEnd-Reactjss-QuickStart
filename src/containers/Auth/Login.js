7cf1552ed34dc1bd8ffcff3f8ff1c051c4d70dbc
Lê Nguyễn Trung Hiếu <lenguyentrunghieu2001@gmail.com>
2023-03-04 11:20:28 +0700

    4/3/2023

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -4,7 +4,7 @@ import { push } from "connected-react-router";
 import * as actions from "../../store/actions";
 import "./Login.scss";
 import { FormattedMessage } from "react-intl";
-
+import { handleLogin } from "../../services/userService";
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -27,7 +27,9 @@ class Login extends Component {
     });
   };
 
-  handleLogin = () => {};
+  handleLogin = async () => {
+    await handleLogin(this.state.username, this.state.password);
+  };
 
   handleShowHidePassword = () => {
     this.setState({
